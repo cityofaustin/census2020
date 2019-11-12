883a409c1dda95dbde855922cda6d5d0f8502780
Mateo Clarke <code@mateoclarke.com>
2019-11-11 23:29:24 -0500

    try to disable typography

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -68,12 +68,12 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sass`,
   ],
