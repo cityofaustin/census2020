9d65501b714a0d2794408d4e7e9fa75ef0461e84
dontehightower <dontedhightower@gmail.com>
2019-11-20 01:42:22 -0600

    add react-helmet fix "serious" react-axe issues

--- gatsby/src/components/index.js
+++ gatsby/src/components/index.js
@@ -1,103 +1,115 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import Img from 'gatsby-image';
 import chunk from 'lodash/fp/chunk';
 import circle from 'uswds_images/circle-124.png';
 import Layout from '../components/layout';
 
 const Index = ({ news, yml, heroImg }) => {
-  const { callout, media, section, tagline, layout } = yml;
-
+  const { callout, media, section, tagline, layout, title } = yml;
   return (
-    <Layout>
-      <section className="usa-hero">
-        <Img fluid={heroImg} className="usa-hero__image" fadeIn={false} />
-        <div className="grid-container">
-          <div className="usa-hero__callout">
-            <h2 className="usa-hero__heading">{callout.title}</h2>
-            <p>{callout.text}</p>
-            <Link className="usa-button" to={callout.cta.link}>
-              {callout.cta.text}
-            </Link>
+    <>
+      <Helmet>
+        <html
+          lang={
+            window.location.pathname.substring(1) === '/'
+              ? 'en'
+              : window.location.pathname.substring(1)
+          }
+        />
+        <title>{callout.title}</title>
+      </Helmet>
+      <Layout>
+        <section className="usa-hero">
+          <Img fluid={heroImg} className="usa-hero__image" fadeIn={false} />
+          <div className="grid-container">
+            <div className="usa-hero__callout">
+              <h2 className="usa-hero__heading">{callout.title}</h2>
+              <p>{callout.text}</p>
+              <Link className="usa-button" to={callout.cta.link}>
+                {callout.cta.text}
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="grid-container usa-section">
-        <div className="grid-row grid-gap">
-          <div className="tablet:grid-col-4">
-            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
-              {tagline.title}
-            </h2>
-          </div>
-          <div className="tablet:grid-col-8 usa-prose">
-            {tagline.content.map((p, idx) => (
-              <p key={idx}>{p}</p>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <section className="grid-container usa-section">
-        <div className="grid-row grid-gap">
-          <div className="tablet:grid-col-4">
-            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
-              {layout.latestNews}
-            </h2>
-          </div>
-          <div className="tablet:grid-col-8 usa-prose">
-            <ul>
-              {news.map((
-                newsPost,
-                i //   TODO               //
-              ) => (
-                // Make this actaully link
-                <li
-                  key={`news-${i}`}
-                  style={{ color: 'blue', textDecoration: 'underline' }}
-                >
-                  <h2 className="font-heading-l margin-top-0">
-                    {newsPost.frontmatter.title}
-                  </h2>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </section>
-
-      <section className="usa-graphic-list usa-section usa-section--dark">
-        <div className="grid-container">
-          {chunk(2, media).map((pairs, idx) => (
-            <div key={idx} className="usa-graphic-list__row grid-row grid-gap">
-              {pairs.map(({ title, text }, idx) => (
-                <div key={idx} className="usa-media-block tablet:grid-col">
-                  <img
-                    className="usa-media-block__img"
-                    src={circle}
-                    alt="circle"
-                  />
-                  <div className="usa-media-block__body">
-                    <h3 className="usa-graphic-list__heading">{title}</h3>
-                    <p>{text}</p>
-                  </div>
-                </div>
+        <section className="grid-container usa-section">
+          <div className="grid-row grid-gap">
+            <div className="tablet:grid-col-4">
+              <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+                {tagline.title}
+              </h2>
+            </div>
+            <div className="tablet:grid-col-8 usa-prose">
+              {tagline.content.map((p, idx) => (
+                <p key={idx}>{p}</p>
               ))}
             </div>
-          ))}
-        </div>
-      </section>
+          </div>
+        </section>
 
-      <section className="usa-section">
-        <div className="grid-container">
-          <h2 className="font-heading-xl margin-y-0">{section.title}</h2>
-          <p className="usa-intro">{section.text}</p>
-          <Link className="usa-button usa-button--big" to={section.cta.link}>
-            {section.cta.text}
-          </Link>
-        </div>
-      </section>
-    </Layout>
+        <section className="grid-container usa-section">
+          <div className="grid-row grid-gap">
+            <div className="tablet:grid-col-4">
+              <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+                {layout.latestNews}
+              </h2>
+            </div>
+            <div className="tablet:grid-col-8 usa-prose">
+              <ul>
+                {news.map((
+                  newsPost,
+                  i //   TODO               //
+                ) => (
+                  // Make this actaully link
+                  <li
+                    key={`news-${i}`}
+                    style={{ color: 'blue', textDecoration: 'underline' }}
+                  >
+                    <h2 className="font-heading-l margin-top-0">
+                      {newsPost.frontmatter.title}
+                    </h2>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section className="usa-graphic-list usa-section usa-section--dark">
+          <div className="grid-container">
+            {chunk(2, media).map((pairs, idx) => (
+              <div key={idx} className="usa-graphic-list__row grid-row grid-gap">
+                {pairs.map(({ title, text }, idx) => (
+                  <div key={idx} className="usa-media-block tablet:grid-col">
+                    <img
+                      className="usa-media-block__img"
+                      src={circle}
+                      alt="circle"
+                    />
+                    <div className="usa-media-block__body">
+                      <h3 className="usa-graphic-list__heading">{title}</h3>
+                      <p>{text}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section className="usa-section">
+          <div className="grid-container">
+            <h2 className="font-heading-xl margin-y-0">{section.title}</h2>
+            <p className="usa-intro">{section.text}</p>
+            <Link className="usa-button usa-button--big" to={section.cta.link}>
+              {section.cta.text}
+            </Link>
+          </div>
+        </section>
+      </Layout>
+    </>
   );
 };
 
