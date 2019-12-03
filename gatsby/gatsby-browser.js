a7ce937116606de39ba729d13f149797173e7388
Amberley Romo <amberleyjohanna@gmail.com>
2019-12-02 19:52:25 -0600

    remove react-axe plugin, move react-axe auditing to gatsby-browser

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,9 +1,25 @@
-module.exports = {
-	onClientEntry: () => {
-		try {
-		  require('uswds_polyfills');
-		} catch (e) {
-		  // do nothing
-		}
-	}
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const onClientEntry = () => {
+  try {
+    require('uswds_polyfills');
+  } catch (e) {
+    // do nothing
+  }
+};
+
+// Can re-install gatsby-plugin-react-axe on updated publish
+// https://github.com/angeloashmore/gatsby-plugin-react-axe/pull/1
+export const onInitialClientRender = async (_, pluginOptions = {}) => {
+  const { showInProduction, axeOptions } = {
+    showInProduction: false,
+    axeOptions: {},
+    ...pluginOptions,
+  };
+
+  if (process.env.NODE_ENV === 'development' || showInProduction) {
+    const { default: axe } = await import('react-axe');
+    axe(React, ReactDOM, 1000, axeOptions);
+  }
 };
