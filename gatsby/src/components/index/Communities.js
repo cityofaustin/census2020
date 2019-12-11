99f56738daf44303a30a95ab1bee3f9b1137bf9e
Mateo Clarke <code@mateoclarke.com>
2019-12-11 00:18:52 -0600

    update spacing and prettier settings

--- gatsby/src/components/index/Communities.js
+++ gatsby/src/components/index/Communities.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import { Link } from 'gatsby';
+import React from "react";
+import Img from "gatsby-image";
+import { Link } from "gatsby";
 
-import './communities.css';
+import "./communities.css";
 
 export default function Communities(props) {
   const { imgs } = props;
 
   //   i18n TODO: these titles need to be translated
   const columns = [
-    { title: 'Hispanic / Latino Community', link: '/', img: imgs.latinx },
+    { title: "Hispanic / Latino Community", link: "/", img: imgs.latinx },
     {
-      title: 'Asian-American Community',
-      link: '/',
+      title: "Asian-American Community",
+      link: "/",
       img: imgs.asianAmerican,
     },
     {
-      title: 'Black / African-American Community',
-      link: '/',
+      title: "Black / African-American Community",
+      link: "/",
       img: imgs.africanAmerican,
     },
-    { title: 'Youth & Student Community', link: '/', img: imgs.student },
-    { title: 'Community Experiencing Homelessness', link: '/', img: imgs.student },
+    { title: "Youth & Student Community", link: "/", img: imgs.student },
+    {
+      title: "Community Experiencing Homelessness",
+      link: "/",
+      img: imgs.student,
+    },
   ];
 
   const textStyles = {};
 
   return (
     <section className="bg-primary-dark padding-top-3 padding-bottom-3">
-      <h2 className="text-white text-center font-ui-3xl">Stand up and be counted with your community.</h2>
+      <h2 className="text-white text-center font-ui-3xl">
+        Stand up and be counted with your community.
+      </h2>
       <div className="grid-row padding-y-2 padding-x-105">
         {columns.map((col, i) => (
-          <div class="grid-col-12 tablet:grid-col" key={`ccc-${i}`}>
+          <div className="grid-col-12 tablet:grid-col" key={`ccc-${i}`}>
             <Link className="" to={col.link}>
               <div className="bg-base-darkest margin-1">
                 <Img
--- gatsby/src/components/index/Timeline.js
+++ gatsby/src/components/index/Timeline.js
@@ -1,81 +1,79 @@
-import React from 'react'
+import React from "react";
 
 import {
-    VerticalTimeline,
-    VerticalTimelineElement,
-} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
-import MailIcon from '@material-ui/icons/Mail'
-import StarIcon from '@material-ui/icons/Star'
-import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList'
+import MailIcon from "@material-ui/icons/Mail";
+import StarIcon from "@material-ui/icons/Star";
+import FeaturedPlayListIcon from "@material-ui/icons/FeaturedPlayList";
 
 // TODO: i18n
 const events = [
-    {
-        date: 'MARCH 12-20',
-        title: 'Invitations Mailed',
-        body:
-            'Invitations to complete the 2020 census questionnaire online will be mailed.',
-        icon: <MailIcon />,
-    },
-    {
-        date: 'MARCH 16-24',
-        title: 'Reminder Letters',
-        body: 'Reminder letters will be mailed.',
-        icon: <MailIcon />,
-    },
-    {
-        date: 'MARCH 26 - APRIL 3',
-        title: 'Reminder Postcard',
-        body: 'Reminder postcard will be mailed.',
-        icon: <FeaturedPlayListIcon />,
-    },
-    {
-        date: 'APRIL 1',
-        title: 'Census Day',
-        body: 'Census Day!',
-        icon: <StarIcon />,
-    },
-    {
-        date: 'APRIL 8-16',
-        title: 'Hard Copy Census Mailed',
-        body: 'Another reminder and hard copy questionnaire will be mailed.',
-        icon: <MailIcon />,
-    },
-    {
-        date: 'APRIL 20-27',
-        title: 'Final Postcards Mailed',
-        body: 'Final postcards will be mailed before an in-person follow-up.',
-        icon: <FeaturedPlayListIcon />,
-    },
-]
+  {
+    date: "MARCH 12-20",
+    title: "Invitations Mailed",
+    body:
+      "Invitations to complete the 2020 census questionnaire online will be mailed.",
+    icon: <MailIcon />,
+  },
+  {
+    date: "MARCH 16-24",
+    title: "Reminder Letters",
+    body: "Reminder letters will be mailed.",
+    icon: <MailIcon />,
+  },
+  {
+    date: "MARCH 26 - APRIL 3",
+    title: "Reminder Postcard",
+    body: "Reminder postcard will be mailed.",
+    icon: <FeaturedPlayListIcon />,
+  },
+  {
+    date: "APRIL 1",
+    title: "Census Day",
+    body: "Census Day!",
+    icon: <StarIcon />,
+  },
+  {
+    date: "APRIL 8-16",
+    title: "Hard Copy Census Mailed",
+    body: "Another reminder and hard copy questionnaire will be mailed.",
+    icon: <MailIcon />,
+  },
+  {
+    date: "APRIL 20-27",
+    title: "Final Postcards Mailed",
+    body: "Final postcards will be mailed before an in-person follow-up.",
+    icon: <FeaturedPlayListIcon />,
+  },
+];
 
 export default function Timeline() {
-    return (
-        <section className="bg-primary-lighter padding-top-3 padding-bottom-3">
-            <h2 className="text-center font-ui-3xl">When is the Census?</h2>
-            <p className="text-center font-serif-xl">
-                Don’t Delay! Be sure to fill out your census form before April
-                30th.
-            </p>
-            <VerticalTimeline>
-                {events.map(event => {
-                    return (
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            date={event.date}
-                            iconStyle={{ background: '#1A4480', color: '#fff' }}
-                            icon={event.icon ? event.icon : <StarIcon />}
-                        >
-                            <h3 className="vertical-timeline-element-title">
-                                {event.title}
-                            </h3>
-                            <div>{event.body}</div>
-                        </VerticalTimelineElement>
-                    )
-                })}
-            </VerticalTimeline>
-        </section>
-    )
+  return (
+    <section className="bg-primary-lighter padding-top-3 padding-bottom-3">
+      <h2 className="text-center font-ui-3xl">When is the Census?</h2>
+      <p className="text-center font-serif-xl">
+        Don’t Delay! Be sure to fill out your census form before April 30th.
+      </p>
+      <VerticalTimeline>
+        {events.map((event, i) => {
+          return (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={event.date}
+              iconStyle={{ background: "#1A4480", color: "#fff" }}
+              icon={event.icon ? event.icon : <StarIcon />}
+              key={`event-${i}`}
+            >
+              <h3 className="vertical-timeline-element-title">{event.title}</h3>
+              <div>{event.body}</div>
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
+    </section>
+  );
 }
