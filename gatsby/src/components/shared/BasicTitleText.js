cedf17662b7878b781d81135d356790e8e3b6a9b
Mateo Clarke <code@mateoclarke.com>
2020-03-19 03:29:37 -0500

    Add volunteer resources section

--- gatsby/src/components/shared/BasicTitleText.js
+++ gatsby/src/components/shared/BasicTitleText.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Img from "gatsby-image";
-import { StaticQuery, graphql } from "gatsby";
+import ReactMarkdown from "react-markdown/with-html";
 
 const BasicTitleText = ({ data, lang }) => {
   console.log(lang, data);
@@ -8,11 +7,19 @@ const BasicTitleText = ({ data, lang }) => {
   return (
     <section className="usa-section grid-container padding-top-3 padding-bottom-3">
       <h2 className="text-center font-heading-xl">{data.title}</h2>
-      <p className="usa-intro">{data.text}</p>
+      <div className="usa-intro grid-row padding-y-4">
+        <div className="grid-col-10 grid-offset-1 grid-row">
+          <ReactMarkdown source={data.text} escapeHtml={false} />
+        </div>
+      </div>
       <div className="grid-row">
         {data.cta && (
           <div className="text-center grid-col">
-            <a className="usa-button usa-button--big" href={data.cta_link}>
+            <a
+              className="usa-button usa-button--big"
+              target="_blank"
+              href={data.cta_link}
+            >
               {data.cta}
             </a>
           </div>
