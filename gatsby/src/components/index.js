741692060d649f965ae6874e64568d76b79f0ada
Mateo Clarke <code@mateoclarke.com>
2020-03-23 11:57:22 -0500

    quick add Census is easy section

--- gatsby/src/components/index.js
+++ gatsby/src/components/index.js
@@ -5,7 +5,7 @@ import circle from "uswds_images/circle-124.png";
 import Layout from "../components/layout";
 import Hero from "./index/Hero";
 import Communities from "./index/Communities";
-import OrganizeMap from "./index/OrganizeMap";
+import BasicTitleText from "./shared/BasicTitleText";
 import Timeline from "./shared/Timeline";
 import QuickLinks from "./index/QuickLinks";
 import MainText from "./index/MainText";
@@ -35,6 +35,23 @@ const Index = ({
   const { title, language } = useHelmetTags(uri, callout);
   const heroImg = language === "en" ? images[0] : images[1];
 
+  const textData = {
+    en: {
+      title: "The 2020 Census is easy.",
+      text:
+        "9 Questions in 9 Minutes. You will answer a simple questionnaire about yourself and everyone who is or will be living with you on April 1, 2020. Your home can fill out the Census in three ways: Online, By Phone, and By Mail.",
+      cta: "Take the Census NOW!",
+      cta_link: "https://my2020census.gov/",
+    },
+    es: {
+      title: "Completar el Censo del 2020 es fácil.",
+      text:
+        "9 preguntas en 9 minutos. Usted responderá a un formulario sencillo que hace preguntas sobre usted y todas las personas que viven en su hogar el 1 de abril del 2020. Su hogar puede llenar el Censo de tres maneras: En línea, Por teléfono, y Por correo.",
+      cta: "Llena el Censo!",
+      cta_link: "https://my2020census.gov/",
+    },
+  };
+
   return (
     <>
       <Helmet>
@@ -51,6 +68,8 @@ const Index = ({
         <Timeline lang={language} />
 
         <Communities lang={language} />
+
+        <BasicTitleText lang={language} data={textData[language]} />
       </Layout>
     </>
   );
