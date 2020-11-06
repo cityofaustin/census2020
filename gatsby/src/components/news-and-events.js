6b5c757db6806f28e41e8425692c4b91a0badc9a
Mateo Clarke <code@mateoclarke.com>
2020-11-06 02:28:43 -0600

    fix language context

--- gatsby/src/components/news-and-events.js
+++ gatsby/src/components/news-and-events.js
@@ -3,24 +3,26 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import Layout from "./layout";
 import NewsAndEvents from "./index/NewsAndEvents";
+import { langs } from "../data/languages";
 
 const propTypes = {
   uri: PropTypes.string,
   location: PropTypes.object,
 };
 
-const Events = ({ location }) => {
+const Events = ({ location, pageContext }) => {
   const title = "Census 2020 News and Events";
-  const language = "en";
+  // const language = "en";
+  const { lang } = pageContext;
 
   return (
     <>
       <Helmet>
-        <html lang={language} />
+        <html lang={lang} />
         <title>{title}</title>
       </Helmet>
-      <Layout language={language} location={location}>
-        <NewsAndEvents lang={language} />
+      <Layout language={lang} location={location}>
+        <NewsAndEvents lang={lang} />
       </Layout>
     </>
   );
